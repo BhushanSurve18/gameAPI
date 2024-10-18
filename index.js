59e4f5b4ee5e0ce570bbd04d652577433f687f01
BhushanSurve18 <bhushsurve1001@gmail.com>
2024-10-17 22:14:29 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { resolve } = require('path');
+const { getAllGames, getGameById } = require('./controllers');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/games', async (req, res) => {
+  const games = getAllGames();
+  res.json({ games });
+});
+
+app.get('/games/details/:id', async (req, res) => {
+  const game = getGameById(parseInt(req.params.id));
+  res.json({ game });
+});
+
+module.exports = { app };
