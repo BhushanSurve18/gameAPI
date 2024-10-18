74e5f913de25503f4dcec2f7ab0d311a672bad6a
BhushanSurve18 <bhushsurve1001@gmail.com>
2024-10-17 22:19:48 -0500

    Changed in the code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { resolve } = require('path');
 const { getAllGames, getGameById } = require('./controllers');
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/games', async (req, res) => {
